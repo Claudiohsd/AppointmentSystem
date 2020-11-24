bc661ccef02ba4c3975881a6e58b45d068c8fbbf
Claudio Dionisio <60439689+Claudiohsd@users.noreply.github.com>
2020-11-24 23:00:59 +0000

    Update MustacheBarbers.java

--- src/mustachebarbers/MustacheBarbers.java
+++ src/mustachebarbers/MustacheBarbers.java
@@ -17,7 +17,7 @@ public class MustacheBarbers {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Claudio Dionisio");
-                System.out.println("Claudio Dionisio");
+                
 
     }
     
