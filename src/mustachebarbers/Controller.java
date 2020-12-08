001a69e1e5d89306f55852dcc005a09560825643
Claudio Dionisio <60439689+Claudiohsd@users.noreply.github.com>
2020-12-08 19:33:51 +0000

    booking system updates with viewBooking page working properly

--- src/mustachebarbers/Controller.java
+++ src/mustachebarbers/Controller.java
@@ -138,10 +138,7 @@ public class Controller implements ActionListener {
             userPage = new UserPage(this);
             vBooking.setVisible(false);
             userPage.setVisible(true);
-        } else {
-            cancelled = true;
-        }
-
+        } 
     }
 
     public void viewBooking() {
